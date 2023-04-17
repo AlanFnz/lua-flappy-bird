60985a463121b28d5a9318b5ea572f53a606444c
Alan Fernandez <alan.f@msn.com>
2023-04-17 19:22:39 -0300

    bird class

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 -- virtual resolution handling library
 push = require 'push'
+Class = require 'class'
+
+require 'Bird'
 
 -- physical screen dimensions
 WINDOW_WIDTH = 1280
@@ -12,6 +15,15 @@ VIRTUAL_HEIGHT = 288
 -- images we load into memory from files to later draw onto the screen
 local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
+local backgroundScroll = 0
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -38,14 +50,22 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+end
+
 function love.draw()
     push:start()
     
     -- draw the background starting at top left (0, 0)
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    bird:render()
     
     push:finish()
 end
