c0e9f82840532d6144c5c95b3486b870158d2991
Alan Fernandez <alan.f@msn.com>
2023-06-12 19:33:34 +0100

    states, state machind and main titles

--- main.lua
+++ main.lua
@@ -14,6 +14,12 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+-- game state and state machines
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/PlayState'
+require 'states/TitleScreenState'
+
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
@@ -32,18 +38,36 @@ local scrolling = true
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
-    -- initialize our nearest-neighbor filter
+    -- initialize nearest-neighbor filter
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- app window title
     love.window.setTitle('Fifty Bird')
 
-    -- initialize our virtual resolution
+    -- initialize virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
     })
+
+    -- initialize text fonts
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function()
+            return TitleScreenState()
+        end,
+        ['play'] = function()
+            return PlayState()
+        end
+    }
+    gStateMachine:change('title')
 
     -- seed 
     math.randomseed(os.time())
@@ -72,46 +96,12 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    if scrolling then
+    -- background scrollin
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 
-        -- background scrollin
-        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-
-        -- pipePairs spawner
-        spawnTimer = spawnTimer + dt
-        if spawnTimer > 2 then
-            local y = math.max(-PIPE_HEIGHT + 10,
-                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-            lastY = y
-            table.insert(pipePairs, PipePair(y))
-            spawnTimer = 0
-        end
-
-        -- bird update
-        bird:update(dt)
-
-        for k, pair in pairs(pipePairs) do
-            pair:update(dt)
-
-            for l, pipe in pairs(pair.pipes) do
-                if bird:collides(pipe) then
-                    -- pause the game to show collision
-                    scrolling = false
-                end
-            end
-
-            if pair.x < -PIPE_WIDTH then
-                pair.remove = true
-            end
-        end
-
-        for k, pair in pairs(pipePairs) do
-            if pair.remove then
-                table.remove(pipePairs, k)
-            end
-        end
-    end
+    -- update the state machine
+    gStateMachine:update(dt)
 
     love.keyboard.keysPressed = {}
 end
@@ -119,18 +109,10 @@ end
 function love.draw()
     push:start()
 
-    -- draw the background starting at top left (0, 0)
+    -- draw state machine between the background and ground
     love.graphics.draw(background, -backgroundScroll, 0)
-
-    -- draw pipePairs
-    for k, pair in pairs(pipePairs) do
-        pair:render()
-    end
-
-    -- draw the ground on top of the background, toward the bottom of the screen
+    gStateMachine:render()
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
-    bird:render()
-
-    push:finish()
+    push:finish() 
 end
